5391929605773df9e42f7d2c40c415d5343b8a56
liyujia <hb.liyujia@aisino.com>
2021-03-22 10:32:22 +0800

    'feat:在主进程中用BrowserView嵌入网页

--- electron-demo/main.js
+++ electron-demo/main.js
@@ -8,6 +8,8 @@ var BrowserWindow = electron.BrowserWindow;  //创建窗口引用
 
 var mainWindow = null ;  //声明要打开的主窗口
 
+
+
 app.on('ready',()=>{
   //webPreferences 启用所有node方法可以在渲染进程使用
   mainWindow = new BrowserWindow({width:400,height:400,webPreferences: {nodeIntegration: true}})   
@@ -22,5 +24,12 @@ app.on('ready',()=>{
   mainWindow.on('closed',()=>{
       mainWindow = null
   })
+  // 用BrowserView嵌入网页，只能放在app ready里面
+  var BrowserView = electron.BrowserView //引入BrowserView
+  var view = new BrowserView()   //new出对象
+  mainWindow.setBrowserView(view)   // 在主窗口中设置view可用
+  view.setBounds({x:0,y:100,width:1200, height:800})  //定义view的具体样式和位置
+  view.webContents.loadURL('https://jspang.com')  //wiew载入
 
 })
+
